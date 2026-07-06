16ec7de832c40c12eb6ff53b2c2fe94eb7f7c1e2
carolusquintus <carloschars@gmail.com>
2026-07-06 07:19:52 -0600

    Fix name parameter to path for ErrorResponseDto

--- bank-account-service/src/main/java/dev/carv/bank/account/dto/ErrorResponseDto.java
+++ bank-account-service/src/main/java/dev/carv/bank/account/dto/ErrorResponseDto.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import java.time.LocalDateTime;
 
 public record ErrorResponseDto(
-    String apiPath,
+    String path,
     HttpStatus errorCode,
     String errorMessage,
     LocalDateTime errorT
