0dfd76df76b459dce3b47a505c3877cc3f4f8543
carolusquintus <carlos.alejandro.rosas@walmart.com>
2026-08-09 14:19:29 -0600

    AccountAPI interface with documentation updated

--- bank-account-service/src/main/java/dev/carv/bank/account/api/AccountAPI.java
+++ bank-account-service/src/main/java/dev/carv/bank/account/api/AccountAPI.java
@@ -2,6 +2,8 @@ package dev.carv.bank.account.api;
 
 import dev.carv.bank.account.dto.CustomerDto;
 import dev.carv.bank.account.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
@@ -11,12 +13,76 @@ import org.springframework.http.ResponseEntity;
 )
 public interface AccountAPI {
 
+    @Operation(
+        summary = "Create account endpoint",
+        description = "REST operation to create a new customer and account inside Bank CARV",
+        responses = {
+             @ApiResponse(
+                 responseCode = "201",
+                 description = "Account Created"
+             )
+        }
+    )
     ResponseEntity<ResponseDto> createAccount(CustomerDto dto);
 
+    @Operation(
+        summary = "Update account endpoint",
+        description = "REST operation to update customer and account details based on a account number",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Account Updated"
+            ),
+            @ApiResponse(
+                responseCode = "404",
+                description = "Account Not Found"
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                description = "Internal Server Error"
+            )
+        }
+    )
     ResponseEntity<ResponseDto> updateAccount(CustomerDto dto);
 
+    @Operation(
+        summary = "Delete account endpoint",
+        description = "REST operation to delete customer and account details based on a mobile number",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Account Deleted"
+            ),
+            @ApiResponse(
+                responseCode = "404",
+                description = "Account Not Found"
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                description = "Internal Server Error"
+            )
+        }
+    )
     ResponseEntity<CustomerDto> fetchAccount(String mobileNumber);
 
+    @Operation(
+        summary = "Fetch account endpoint",
+        description = "REST operation to fetch customer and account details based on a mobile number",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Account Deleted"
+            ),
+            @ApiResponse(
+                responseCode = "404",
+                description = "Account Not Found"
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                description = "Internal Server Error"
+            )
+        }
+    )
     ResponseEntity<ResponseDto> deleteAccount(String mobileNumber);
 
 }
