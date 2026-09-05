a6328d2a79bcde650bf4dc1c37184f86213607d0
carolusquintus <carloschars@gmail.com>
2026-09-05 10:50:37 -0600

    Update latency script

--- latency.bash
+++ latency.bash
@@ -1,7 +1,7 @@
-#!/bin/zsh
+#!/bin/bash
 
-docker-compose down
-docker-compose up -d
+docker compose down
+docker compose up -d
 
 sleep 2s
 
@@ -13,7 +13,11 @@ docker exec -it --user root bank-account-db apt-get install -y iproute2
 docker exec -it --user root bank-account-db tc qdisc add dev eth0 root netem delay 1000ms
 
 # change latency
-#docker exec -it --user root bank-account-db tc qdisc change dev eth0 root netem delay 2500ms
+docker exec -it --user root bank-account-db tc qdisc change dev eth0 root netem delay 2500ms
 
 # remove latency
-#docker exec -it --user root bank-account-db tc qdisc del dev eth0 root netem
+docker exec -it --user root bank-account-db tc qdisc del dev eth0 root netem
+
+# Just in case if a port is being used
+#sudo lsof -i TCP:<PORT> -sTCP:LISTEN
+#sudo kill -9 <PID>
